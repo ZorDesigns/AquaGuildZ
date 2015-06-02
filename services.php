81615ec72b712b070e44060a960e3b7af783d8a0
Alex iFailZorD <alex_pap_2010@live.com>
2015-06-03 02:01:57 +0300

    [23] Services Page
    Added he Services Template.
    Nothing Special here. Just designed with Love and Coffee.

---
+++ services.php
@@ -0,0 +1,155 @@
+<?php
+$page_cat = "services";
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>   <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>   <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html>
+<head>
+<meta charset="utf-8">
+<title>AquaFlameCMS Frontpage</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A wow private server made with AquaFlameCMS">
+<meta name="author" content="AquaFlameCMS">
+<!-- Le styles -->
+<link href="assets/stylesheets/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="assets/stylesheets/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+<link href="assets/stylesheets/main.css" rel="stylesheet" type="text/css">
+<link href="assets/stylesheets/forum.css" rel="stylesheet" type="text/css">
+<link href="assets/stylesheets/status.css" rel="stylesheet" type="text/css">
+<!-- Le javascripts -->
+<script src="assets/javascript/jquery.min.js"></script>
+<script src="assets/javascript/jquery.flexslider.min.js"></script>
+<script src="assets/javascript/bootstrap.min.js"></script>
+<script src="assets/javascript/global.js"></script>
+<script src="assets/javascript/common_orig.js"></script>
+</head>
+<body>
+<div id="wrapper">
+<header id="main_header" class="clearfix">
+<?php include("webkit/servicebar") ?>
+<a href="index.php"><div id="logo"></div></a>
+<div id="searchbar">
+<form>
+<input placeholder="Search characters, items, forums and more..." type="text">
+</form>
+</div>
+</header>
+<div id="content" class="clearfix">
+<div id="main">
+<?php include("webkit/menu"); ?>
+<!-- Main Content Add here -->
+<div class="container_5" align="center">
+<div class="content_holder">
+<div class="sub-page-title">
+<div id="title"><h1>Services<p></p><span></span></h1></div>
+</div>
+<div class="feature-row">
+<img src="assets/images/services/raf.jpg" width="90" height="90">
+<div class="info">
+<h1>We are a Family</h1>
+<h2>We are a family and we raid as one. We feel that progress is really important, but members too. We are strict and we will go hard on you if you fail, cause we all love World of Warcraft.</h2>
+</div>
+<div class="clear"></div>
+</div>
+<div class="feature-row">
+<img src="assets/images/services/bug-tracker.jpg" width="90" height="90">
+<div class="info">
+<h1>Guild Score</h1>
+<h2>Our Score is really important and we do want to focus on becoming great. We also have Guides and more features to help you understand your class by a perspective of our loyal members. You can scan your character and check if your avatar is ready for it.</h2>
+</div>
+<div class="clear"></div>
+</div>
+<div class="feature-row">
+<img src="assets/images/services/teleporter.jpg" width="90" height="90">
+<div class="info">
+<h1>Guild Secrets</h1>
+<h2>We are full with Secrets to help you gain a step up in the game. But to do get to those secrets you must prove your self. Raiders and Core Raiders allowed only.</h2>
+</div>
+<div class="clear"></div>
+</div>
+<div class="feature-row">
+<img src="assets/images/services/store.jpg" width="90" height="90">
+<div class="info">
+<h1>Character Fixer</h1>
+<h2>We provide a feature that will balance your character and show you what is best on you to raid with us and to raid better. Also you can follow a Guide that we also provide to understand your role better.</h2>
+</div>
+<div class="clear"></div>
+</div>
+<div class="feature-row">
+<img src="assets/images/services/changelogs.jpg" width="90" height="90">
+<div class="info">
+<h1>Changelogs</h1>
+<h2>
+The changelog page enables you to view our changes that we make everyday...
+Please keep in mind that not all changes are currently applied to the guild or the website, but will be on the next maintenance.
+</h2>
+</div>
+<div class="clear"></div>
+</div>
+<div class="features-bg-dark"></div>
+<div class="features-bg"></div>
+</div>
+<br>
+</div>
+<br>
+</div>
+<div id="sidebar">
+<div id="user_control">
+<a data-toggle="modal" href="#loginframe">
+<div id="user_control_deco"><!-- --></div>
+<p><span>Log in now</span> to enhance and<br>personalize your experience!</p></a> 
+</div>
+</div>
+</div><footer>
+<div id="lower_nav" class="clearfix">
+<nav>
+<ul>
+<li class="header">AquaFlame Home</li>
+<li><a href="">Whats AquaFlame?</a></li>
+<li><a href="">Donate for us</a></li>
+<li><a href="">Account Panel</a></li>
+<li><a href="">Support</a></li>
+<li><a href="">Make free account</a></li>
+</ul>
+</nav>
+<nav>
+<ul>
+<li class="header">Account</li>
+<li><a href="">Forgot Password?</a></li>
+<li><a href="">Go Premium/Vip</a></li>
+<li><a href="">Forum User CP</a></li>
+<li><a href="">Game User CP</a></li>
+<li><a href="">Report Abuse</a></li>
+</ul>
+</nav>
+<nav>
+<ul>
+<li class="header">Support</li>
+<li><a href="">OMFG I got hacked</a></li>
+<li><a href="">Banned, why?</a></li>
+<li><a href="">Forums</a></li>
+<li><a href="">FAQ</a></li>
+<li><a href="">Rules</a></li>
+</ul>
+</nav>
+<div id="logos">
+<a href="#"><span>AquaFlameCMS</span></a>
+</div>
+</div>
+<div id="credits">
+<nav class="pull-right">
+<ul>
+<li><a href="">Terms of Use</a></li>
+<li><a href="">Legal</a></li>
+<li><a href="">Privacy Policy</a></li>
+</ul>
+</nav>
+&copy; 2012 AquaFlame, INC. All rights reserved
+</div>
+</footer>
+</div>
+</body>
+</html>
